0319638e3bb307fb891355a52c515c704838457e
AbrehamElka <abrehamdessalegn02@gmail.com>
2026-03-05 12:06:53 +0300

    Error Handling setup

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
 import express from "express";
-import { logger } from "./middlewares/logger";
-import { env } from "./config/env";
+// import { logger } from "./middlewares/logger";
+// import { env } from "./config/env";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import { errorHandler } from "./middlewares/errorHandler";
 
+dotenv.config();
 const app = express();
+const logger = require("morgan");
 
-app.use(logger);
+app.use(helmet());
+app.use(logger("dev"));
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-app.listen(env.port, () => {
-  console.log(`Running On http://localhost:${env.port}`);
+app.use(errorHandler);
+app.listen(process.env.PORT, () => {
+  console.log(`Running On http://localhost:${process.env.PORT}`);
 });
